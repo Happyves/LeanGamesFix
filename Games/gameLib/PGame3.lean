7872cd5642c4cc0b2597e2dce5f827429ad14c56
Happyves <yves-j@gmx.de>
2024-11-27 19:40:18 +0100

    rgrgm

--- Games/gameLib/PGame3.lean
+++ Games/gameLib/PGame3.lean
@@ -56,95 +56,102 @@ def PGameI (g : PGame) : Game_World PGame (MoveType) where
   init_game_state := g
   fst_win_states := fun hist => ∃ pre, pre <:+ hist ∧ Turn_snd pre.length ∧ PGame_of_hist g hist = 0
   snd_win_states := fun hist => ∃ pre, pre <:+ hist ∧ Turn_fst pre.length ∧ PGame_of_hist g hist = 0
-  fst_legal := fun hist act => if PGame_of_hist g hist = 0 then act = .over else legalRight g hist act
-  snd_legal := fun hist act => if PGame_of_hist g hist = 0 then act = .over else legalLeft g hist act
+  fst_legal := fun hist act => if ¬ Nonempty (PGame_of_hist g hist).RightMoves then act = .over else legalRight g hist act
+  snd_legal := fun hist act => if ¬ Nonempty (PGame_of_hist g hist).LeftMoves then act = .over else legalLeft g hist act
   fst_transition := fun hist act => PGame_of_hist g (act :: hist)
   snd_transition := fun hist act => PGame_of_hist g (act :: hist)
 
 
-lemma PGame_zero_cons (g : PGame) (hist : List MoveType) (act : MoveType) (leg : (PGameI g).hist_legal (act :: hist))
-  (zero : (PGame_of_hist g hist) = 0) : (PGame_of_hist g (act :: hist)) = 0 :=
-    by
-    cases' leg
-    rename_i sofar now
-    split_ifs at now
-    · dsimp [PGameI] at now
-      rw [if_pos zero] at now
-      rw [now]
-      dsimp [PGame_of_hist]
-    · dsimp [PGameI] at now
-      rw [if_pos zero] at now
-      rw [now]
-      dsimp [PGame_of_hist]
+-- lemma PGame_zero_cons (g : PGame) (hist : List MoveType) (act : MoveType) (leg : (PGameI g).hist_legal (act :: hist))
+--   (zero : (PGame_of_hist g hist) = 0) : (PGame_of_hist g (act :: hist)) = 0 :=
+--     by
+--     cases' leg
+--     rename_i sofar now
+--     split_ifs at now
+--     · dsimp [PGameI] at now
+--       rw [if_pos zero] at now
+--       rw [now]
+--       dsimp [PGame_of_hist]
+--     · dsimp [PGameI] at now
+--       rw [if_pos zero] at now
+--       rw [now]
+--       dsimp [PGame_of_hist]
 
 
-lemma PGame_zero_suffix (g : PGame) (pre hist : List MoveType) (suff : pre <:+ hist) (leg : (PGameI g).hist_legal (hist))
-  (zero : (PGame_of_hist g pre) = 0) : (PGame_of_hist g hist) = 0 :=
-    by
-    induction' hist with last post ih
-    · rw [List.suffix_nil] at suff
-      rw [suff] at zero
-      apply zero
-    · rw [List.suffix_cons_iff] at suff
-      cases' suff with q q
-      · rw [← q]
-        apply zero
-      · apply PGame_zero_cons _ _ _ leg
-        apply ih q
-        cases' leg
-        rename_i sofar _
-        exact sofar
+-- lemma PGame_zero_suffix (g : PGame) (pre hist : List MoveType) (suff : pre <:+ hist) (leg : (PGameI g).hist_legal (hist))
+--   (zero : (PGame_of_hist g pre) = 0) : (PGame_of_hist g hist) = 0 :=
+--     by
+--     induction' hist with last post ih
+--     · rw [List.suffix_nil] at suff
+--       rw [suff] at zero
+--       apply zero
+--     · rw [List.suffix_cons_iff] at suff
+--       cases' suff with q q
+--       · rw [← q]
+--         apply zero
+--       · apply PGame_zero_cons _ _ _ leg
+--         apply ih q
+--         cases' leg
+--         rename_i sofar _
+--         exact sofar
 
 
-lemma PGame_ne_zero_of_cons (g : PGame) (hist : List MoveType) (act : MoveType) (leg : (PGameI g).hist_legal (act :: hist))
-  (zero : (PGame_of_hist g (act :: hist)) ≠ 0) : (PGame_of_hist g hist) ≠ 0 :=
-    by
-    contrapose! zero
-    apply PGame_zero_cons _ _ _ leg zero
+-- lemma PGame_ne_zero_of_cons (g : PGame) (hist : List MoveType) (act : MoveType) (leg : (PGameI g).hist_legal (act :: hist))
+--   (zero : (PGame_of_hist g (act :: hist)) ≠ 0) : (PGame_of_hist g hist) ≠ 0 :=
+--     by
+--     contrapose! zero
+--     apply PGame_zero_cons _ _ _ leg zero
 
 
-lemma PGame_ne_zero_of_cons_ok (g : PGame) (hist : List MoveType) (type : Type u) (move : type)
-  (leg : (PGameI g).hist_legal ((.ok type move) :: hist)) : (PGame_of_hist g hist) ≠ 0 :=
-    by
-    cases' leg
-    rename_i sofar now
-    split_ifs at now
-    · dsimp [PGameI] at now
-      split_ifs at now
-      rename_i goal
-      exact goal
-    · dsimp [PGameI] at now
-      split_ifs at now
-      rename_i goal
-      exact goal
+-- lemma PGame_ne_zero_of_cons_ok (g : PGame) (hist : List MoveType) (type : Type u) (move : type)
+--   (leg : (PGameI g).hist_legal ((.ok type move) :: hist)) : (PGame_of_hist g hist) ≠ 0 :=
+--     by
+--     cases' leg
+--     rename_i sofar now
+--     split_ifs at now
+--     · dsimp [PGameI] at now
+--       split_ifs at now
+--       rename_i goal
+--       exact goal
+--     · dsimp [PGameI] at now
+--       split_ifs at now
+--       rename_i goal
+--       exact goal
 
+
+-- #exit
 
 lemma type_left_of_turn_fst (g : PGame) (hist : List MoveType) (type : Type u) (move : type)
   (leg : (PGameI g).hist_legal ((.ok type move) :: hist)) (T : Turn_fst (((.ok type move) :: hist).length + 1)) :
   type = (PGame_of_hist g hist).LeftMoves :=
     by
     rw [List.length_cons] at T
-    have leg' := leg
     cases' leg
     rename_i sofar now
     rw [← Turn_fst_step] at T
     rw [if_neg (by rw [← Turn_fst_not_step] ; exact T)] at now
     dsimp [PGameI, legalLeft] at now
-    rw [if_neg (PGame_ne_zero_of_cons_ok g hist type move leg')] at now
-    exact now
+    rw [if_neg] at now
+    · exact now
+    · intro con
+      rw [if_pos con] at now
+      contradiction
+
 
 lemma type_right_of_turn_snd (g : PGame) (hist : List MoveType) (type : Type u) (move : type)
   (leg : (PGameI g).hist_legal ((.ok type move) :: hist)) (T : Turn_snd (((.ok type move) :: hist).length + 1)) :
   type = (PGame_of_hist g hist).RightMoves :=
     by
     rw [List.length_cons] at T
-    have leg' := leg
     cases' leg
     rename_i sofar now
     rw [if_pos (by rw [Turn_fst_snd_step] ; exact T)] at now
     dsimp [PGameI, legalRight] at now
-    rw [if_neg (PGame_ne_zero_of_cons_ok g hist type move leg')] at now
-    exact now
+    rw [if_neg] at now
+    · exact now
+    · intro con
+      rw [if_pos con] at now
+      contradiction
 
 
 
@@ -162,12 +169,12 @@ def preLeftWinStrat (g : PGame) :
     | last :: before =>
         match last with
         | .over =>
-          ⟨.over, And.intro (by dsimp [PGameI, PGame_of_hist] ; rw [if_pos rfl]) (by dsimp [PGame_of_hist] ; exact zero_le_of_isEmpty_rightMoves 0)⟩
+          ⟨.over, And.intro (by dsimp [PGameI, PGame_of_hist] ; rw [if_pos not_nonempty_pempty]) (by dsimp [PGame_of_hist] ; exact zero_le_of_isEmpty_rightMoves 0)⟩
         | .ok type move =>
             have R := type_right_of_turn_snd g before type move leg T
             let next := Classical.choose (PGame.zero_le.mp (H before (by exact List.suffix_cons (MoveType.ok type move) before) (by rw [Turn_snd_step] ; apply T)) (by rw [← R] ; exact move))
             let nextP := Classical.choose_spec (PGame.zero_le.mp (H before (by exact List.suffix_cons (MoveType.ok type move) before) (by rw [Turn_snd_step] ; apply T)) (by rw [← R] ; exact move))
-            if O : PGame_of_hist g ((.ok type move) :: before) = 0
+            if O : ¬ Nonempty (PGame_of_hist g (MoveType.ok type move :: before)).LeftMoves
             then -- shouldn't happen though
               ⟨.over, And.intro (by dsimp [PGameI] ; rw [if_pos O]) (by dsimp [PGame_of_hist] ; exact zero_le_of_isEmpty_rightMoves 0)⟩
             else
@@ -194,16 +201,16 @@ def preLeftWinStrat (g : PGame) :
 #check 1
 
 noncomputable
-def leftWinStrat (g : PGame) (hg : 0 ≤ g) : (PGameI g).sStrategy :=
+def leftWinStrat (g : PGame) : (PGameI g).sStrategy :=
   fun hist T leg =>
     match hist with
     | [] => (by contradiction)
     | last :: before =>
         match last with
         | .over =>
-          ⟨.over, (by dsimp [PGameI, PGame_of_hist] ; rw [if_pos rfl])⟩
+          ⟨.over, (by dsimp [PGameI, PGame_of_hist] ; rw [if_pos not_nonempty_pempty])⟩
         | .ok type move =>
-            if O : PGame_of_hist g ((.ok type move) :: before) = 0
+            if O : ¬ Nonempty (PGame_of_hist g (MoveType.ok type move :: before)).LeftMoves
             then -- shouldn't happen though
               ⟨.over, (by dsimp [PGameI] ; rw [if_pos O])⟩
             else
@@ -211,7 +218,6 @@ def leftWinStrat (g : PGame) (hg : 0 ≤ g) : (PGameI g).sStrategy :=
               then
                 have R := type_right_of_turn_snd g before type move leg T
                 let next := Classical.choose (PGame.zero_le.mp (H before (by exact List.suffix_cons (MoveType.ok type move) before) (by rw [Turn_snd_step] ; apply T)) (by rw [← R] ; exact move))
-                let nextP := Classical.choose_spec (PGame.zero_le.mp (H before (by exact List.suffix_cons (MoveType.ok type move) before) (by rw [Turn_snd_step] ; apply T)) (by rw [← R] ; exact move))
                 have help : moveRight (PGame_of_hist g before) (cast (@Eq.symm (Type _) (RightMoves (PGame_of_hist g before)) type
                   (R.symm ▸ Eq.refl (RightMoves (PGame_of_hist g before)))) move) =
                   PGame_of_hist g (MoveType.ok type move :: before) :=
@@ -220,6 +226,13 @@ def leftWinStrat (g : PGame) (hg : 0 ≤ g) : (PGameI g).sStrategy :=
                     rw [dif_neg (by apply not_and_of_not_right ; rw [Turn_snd_not_step, not_not] ; apply T ), dif_pos ⟨R, (by rw [Turn_fst_snd_step] ; apply T)⟩]
                 ⟨MoveType.ok _ next,(by dsimp [PGameI] ; rw [if_neg O] ; dsimp [legalLeft] ; rw [help])⟩
               else -- shouldn't happen though
-                _
+                by
+                rw [not_not] at O
+                exact ⟨MoveType.ok _ (Classical.choice O),(by dsimp [PGameI] ; rw [if_neg (by rw [not_not] ; exact O)] ; dsimp [legalLeft])⟩
                 -- plan is to use a dummy move of type
                 -- (PGame_of_hist ini (MoveType.ok type move :: before)).LeftMoves
+
+
+#check Empty
+#check Nonempty
+#check Classical.choice
